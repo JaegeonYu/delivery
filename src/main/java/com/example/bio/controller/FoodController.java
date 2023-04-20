7bea029d860735d4c7fe557f4bd0c9d97e7f82c2
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-04-20 17:14:23 +0900

    Feat: FoodController find, save 기능 구현

--- src/main/java/com/example/bio/controller/FoodController.java
+++ src/main/java/com/example/bio/controller/FoodController.java
@@ -1,13 +1,27 @@
 package com.example.bio.controller;
 
+import com.example.bio.domain.dto.FoodRequest;
+import com.example.bio.domain.dto.FoodResponse;
 import com.example.bio.service.FoodService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import static org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/foods")
 public class FoodController {
     private final FoodService productService;
 
-    public
+    @PostMapping()
+    public Long save(@RequestBody FoodRequest foodRequest) {
+        return productService.save(foodRequest.toEntity());
+    }
+
+    @GetMapping("/{foodId}")
+    public FoodResponse get(@PathVariable Long foodId) throws NotFoundException {
+        return productService.findById(foodId).toDto();
+    }
+
+
 }
