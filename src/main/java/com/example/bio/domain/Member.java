e8de4ea7f159a1dc833eb0af190cfb05497d9572
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-04-25 17:15:40 +0900

    Feat: Member 클래스 생성

---
+++ src/main/java/com/example/bio/domain/Member.java
@@ -0,0 +1,20 @@
+package com.example.bio.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+}
