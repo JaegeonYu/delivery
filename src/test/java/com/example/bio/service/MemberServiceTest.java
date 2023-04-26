81801c98ccd64b239edb1b833a8dd6cedacb0bae
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-04-26 16:56:45 +0900

    Feat: MemberService joinDuplicateTest

--- src/test/java/com/example/bio/service/MemberServiceTest.java
+++ src/test/java/com/example/bio/service/MemberServiceTest.java
@@ -4,6 +4,7 @@ import com.example.bio.domain.Address;
 import com.example.bio.domain.Member;
 import com.example.bio.repository.MemberRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,9 +23,13 @@ class MemberServiceTest {
         this.memberService = memberService;
         this.memberRepository = memberRepository;
     }
+    @BeforeEach
+    public void beforeEach(){
+        memberRepository.deleteAll();
+    }
 
     @Test
-    @DisplayName("회원가입 ")
+    @DisplayName("회원가입 성공")
     public void joinTest(){
         Member member = Member.builder()
                 .name("bebe")
@@ -37,5 +42,32 @@ class MemberServiceTest {
         Long savedId = memberService.join(member);
 
         Assertions.assertEquals(member, memberRepository.findById(savedId).get());
+    }
+
+    @Test
+    @DisplayName("회원가입 중복예외")
+    public void joinDuplicateTest(){
+        Member member = Member.builder()
+                .name("bebe")
+                .address(Address.builder()
+                        .city("대전")
+                        .street("도로명")
+                        .zipcode("33333")
+                        .build()).build();
+
+        Member nameSamemember = Member.builder()
+                .name("bebe")
+                .address(Address.builder()
+                        .city("서울")
+                        .street("도로명")
+                        .zipcode("11111")
+                        .build()).build();
+
+        memberService.join(member);
+
+        Throwable exception = assertThrows(RuntimeException.class,
+                () -> memberService.join(nameSamemember));
+
+        assertEquals("이미 존재하는 회원", exception.getMessage());
     }
 }
